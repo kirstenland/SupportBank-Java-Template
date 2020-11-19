0d2f7ef19ab3e66c5df5c44daa3d290fc5cd8cd5
WendiF <wendi.fan@softwire.com>
2020-11-19 11:04:49 +0000

    CSV Reading Done

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,27 @@
 package training.supportbank;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+        List<List<String>> transactions = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("Transactions2014.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(", ");
+                transactions.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done!");
     }
 }
